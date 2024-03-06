8e2c72cf33c6df685964812ca544aca23cf0d086
ianssogothis <mountian88@gmail.com>
2024-03-06 18:44:38 -0500

    have Form comp rendering through / SkillListComp rendering throu app.js all imports and exports current

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import MainPage from "./MainPage";
+import Form from "./Form";
+
+
+export default function App() {
+  return (
+    <div> 
+      <h1>React-Dev-Skills</h1>
+      <MainPage />
+      <Form />
+    </div>
+  );
+}
---
+++ src/SkillList.jsx
@@ -0,0 +1,14 @@
+import Form from "./form";
+
+export default function SkillListItem() {
+    return (
+      <div>
+        <ul>
+          <li className="SkillListItem">Learn React</li>
+          <li className="SkillListItem">Learn the MERN-Stack</li>
+        </ul>
+        <Form />
+       
+      </div>
+    );
+  }
